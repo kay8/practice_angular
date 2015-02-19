36c7af66c2a5cab614f8f4d0343035fef31d39bf
kay8 <kay.yamagishi.work@gmail.com>
2015-02-18 23:35:09 -0800

    add

---
+++ egghead/22.js
@@ -0,0 +1,11 @@
+var app = angular.module("phoneApp", []);
+
+app.controller("AppCtrl", function($scope) {
+
+	this.sayHi = function() {
+		alert("Hi");
+	};
+
+	return $scope.AppCtrl = this;
+
+});
