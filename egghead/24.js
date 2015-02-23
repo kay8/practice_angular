c211411fc9fc42cc2f937ead8f7b38a08a5c5d56
kay8 <kay.yamagishi.work@gmail.com>
2015-02-23 00:26:59 -0800

    add

---
+++ egghead/24.js
@@ -0,0 +1,17 @@
+var app = angular.module("app", []);
+app.directive("zippy", function(){
+	return {
+		restrict: "E",
+		transclude: true,
+		scope:{
+			title: "@"
+		},
+		template: '<div><h3 ng-click="toggleContent()">{{title}}</h3>\n<div ng-show="isContentVisible" ng-transclude></div>\n</div>',
+		link: function(scope) {
+			scope.isContentVisible = false;
+			scope.toggleContent = function() {
+				scope.isContentVisible = !scope.isContentVisible;
+			};
+		}
+	};
+});
